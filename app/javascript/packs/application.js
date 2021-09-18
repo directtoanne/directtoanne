c0cd2bdbe89039f500ddb9b06a47edff11b50301
Anne Wong <directtoanne@gmail.com>
2021-09-18 23:04:23 +0100

    dunno what the difference was but it's finally fuckin working yippe kayee motherfucker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,8 +31,116 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  particlesJS('particles-js', 'particlesjs-config.json');
+  particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 66,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#e8ce54"
+      },
+      "shape": {
+        "type": "star",
+        "stroke": {
+          "width": 4,
+          "color": "#f7e665"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 9.110889110889111,
+          "opacity_min": 0.5114885114885115,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 0.7,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 7.192807192807193,
+          "size_min": 2.3976023976023977,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 160,
+        "color": "#e1d62e",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.3,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 1262.6960138265217,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 731.2687312687312,
+          "line_linked": {
+            "opacity": 0.8093889113719145
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 28.403327246906088,
+          "duration": 9.575978900385481,
+          "opacity": 0.6329884357881927,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 154.18949076891874,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 });
 
-particlesJS('particles-js', 'particlesjs-config.json');
 
